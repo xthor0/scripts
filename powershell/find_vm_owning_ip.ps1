5c030462ff5bfd63990937d56f8ece23a0ad17c4
Ben Brown <xthor@xthorsworld.com>
2018-05-08 13:54:54 -0600

    This script will find out which VM owns an IP address. Of course, you can just search in the web interface now...

--- powershell/find_vm_owning_ip.ps1
+++ powershell/find_vm_owning_ip.ps1
@@ -1,3 +1,22 @@
+param(
+    [Parameter(Mandatory = $true)][String] $ipaddress
+)
+
 write-host "Retrieving all VMs... please wait..."
 $vmlist = Get-VM
 
+write-host "Searching all VMs for IP address: $($ipaddress)"
+
+foreach ($vm in $vmlist) {
+  # write-host "Processing $($vm)..."
+  $guestinfo = Get-VMGuest $vm
+  foreach ($guestip in $guestinfo.IPAddress) {
+    if ($guestip -eq $ipaddress) {
+      write-host "Found IP on VM: $($vm)"
+      write-host -nonewline "."
+    } else {
+      # write-host "No Match: $($guestip)"
+      write-host -nonewline "."
+    }
+  }
+}
