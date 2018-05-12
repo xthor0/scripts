51e2258c332adb63f7c3511483c446a9fed75b7c
Ben Brown <xthor@spinne.xthorsworld.com>
2018-05-11 22:19:33 -0600

    compton/openbox/tint2 stuff

---
+++ bash/install_openbox_fedora.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# just a filler in case I want to do this again
+# best performed on top of an LXQt base install
+
+sudo dnf install tint2 obconf compton ImageMagick volumeicon lxappearance nitrogen i3lock scrot xss-lock xautolock lxappearance volumeicon
+
+# find the compton.conf and openbox autostart in the dotfiles directory - should be all you need
