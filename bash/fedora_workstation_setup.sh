2c5629b199a0b2bcb475e9aca2729a7ed6c5dfc7
Ben Brown <xthor@xthorsworld.com>
2018-06-12 22:12:59 -0600

    Modifications for F28 Gnome

--- bash/fedora_workstation_setup.sh
+++ bash/fedora_workstation_setup.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
-# this script needs some work. At various points sudo privs could time out, and the user will be prompted again for a password
-# we need to tell the user what we're doing at every step so that IF they are prompted, it isn't confusing as to why.
+# make sure we're running this as a non-root user...
+if [ "$(whoami)" == "root" ]; then
+    echo "You should not run this script as root - instead, it will invoke commands"
+    echo "using 'sudo' where necessary."
+    exit 255
+fi
 
-# we need sudo rights...
-echo "This script requires sudo rights - let's check your permissions."
-echo "Please enter your password (running: 'sudo whoami'): "
-sudo whoami
-if [ $? -ne 0 ]; then
-	echo "Error - check your sudo permissions and/or password!"
-	exit 255
+# make sure we have sudo rights
+echo "Checking to make sure your account has 'sudo' privileges..."
+echo "Script will execute 'sudo whoami' - please enter your password if prompted."
+if [ "$(sudo whoami)" != "root" ]; then
+    echo "Exiting! The output of 'sudo whoami' did not come back with the string 'root'."
+    exit 255
 fi
 
 # add additional packages that don't come with Fedora by default
+echo "Installing rpmfusion release pacakges..."
 sudo su -c 'dnf install http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm'
 
 # google fedora repo
+echo "Installing official Google Chrome repository..."
 cat << EOF | sudo tee /etc/yum.repos.d/google-chrome.repo
 [google-chrome]
 name=google-chrome
@@ -26,57 +31,80 @@ gpgkey=https://dl-ssl.google.com/linux/linux_signing_key.pub
 EOF
 
 # virtualbox fedora repo
+echo "Installing VirtualBox official repo..."
 curl http://download.virtualbox.org/virtualbox/rpm/fedora/virtualbox.repo | sudo tee /etc/yum.repos.d/virtualbox.repo
 
 # improve fonts
-sudo dnf copr enable dawid/better_fonts
+echo "Installing copy dawid/better_fonts..."
+sudo dnf -y copr enable dawid/better_fonts
 
 # pycharm IDE for Python development
-sudo dnf copr enable phracek/PyCharm 
+echo "Installing copr for PyCharm..."
+sudo dnf -y copr enable phracek/PyCharm 
 
 # spotify
+echo "Installing Spotify repo..."
 sudo dnf config-manager --add-repo=https://negativo17.org/repos/fedora-spotify.repo
 
+# fedora-multimedia gets you makemkv!
+echo "Installing Fedora Multimedia repo..."
+sudo dnf config-manager --add-repo=https://negativo17.org/repos/fedora-multimedia.repo
+
 # do a full upgrade
+echo "Performing full dnf upgrade..."
 sudo dnf -y upgrade
 
-# install all the packages
-sudo dnf -y install vim-enhanced nmap vim-X11 conky lynx axel freerdp terminator expect ncdu pwgen google-chrome-stable VirtualBox-5.2 vlc terminus-fonts-console terminus-fonts kernel-devel fontconfig-enhanced-defaults fontconfig-font-replacements pycharm-community spotify remmina-plugins-rdp arc-theme htop exfat-utils fuse-exfat nautilus-dropbox chrome-gnome-shell telegram-desktop git
+# TODO: if 'dnf upgrade' installs a new kernel, virtualbox will have issues running /sbin/vboxconfig!
+# I need to put in a check here that tells the user to reboot if there's a new kernel.
 
-# atom editor
-# copr repo does not work with F27
-#sudo dnf copr enable mosquito/atom
-#wget https://atom.io/download/rpm -- maybe?
+echo "Checking to see if kernel was upgraded..."
+current_kernel=$(uname -r)
+new_kernel=$(sudo grubby --default-kernel | cut -b 15-)
 
-chassis=$(sudo dmidecode --string chassis-type)
-if [ $chassis == "Notebook" ]; then
-	# install tlp/powertop
-	sudo dnf -y install tlp powertop
-
-	# start powertop and tlp at boot
-	sudo systemctl enable powertop
-	sudo systemctl enable tlp
-	sudo systemctl start powertop
-	sudo systemctl start tlp
+if [ ${current_kernel} != ${new_kernel} ]; then
+	echo "A new kernel was installed! We need to reboot before proceeding."
+	echo "Please run this script again after rebooting."
+	read -n1 -s -r -p "Press any key to reboot..."
+	reboot
 fi
 
-if [ $chassis == "Desktop" ]; then
-	# sshd if you've got a need
-	sudo systemctl enable sshd
-	sudo systemctl start sshd
+# install all the packages
+echo "Installing a bunch of packages..."
+sudo dnf -y install vim-enhanced nmap vim-X11 conky lynx axel freerdp terminator expect ncdu pwgen google-chrome-stable VirtualBox-5.2 vlc kernel-devel fontconfig-enhanced-defaults fontconfig-font-replacements telegram-desktop elfutils-libelf-devel fuse-exfat htop pycharm-community spotify remmina-plugins-rdp arc-theme htop exfat-utils nautilus-dropbox chrome-gnome-shell telegram-desktop git gnome-tweaks makemkv
+
+## ONLY NECESSARY FOR LAPTOPS
+chassistype=$(hostnamectl status | grep Chassis | awk '{ print $2 }')
+if [ "$chassistype" == "laptop" ]; then
+    # install tlp/powertop
+    echo "Installing TLP & powertop (chassis is ${chassistype})..."
+    sudo dnf -y install tlp powertop
+
+    # start powertop and tlp at boot
+    sudo systemctl enable powertop
+    sudo systemctl enable tlp
+    sudo systemctl start powertop
+    sudo systemctl start tlp
+else
+    # for non-laptops - enable sshd
+    echo "Enabling SSHD so it starts at boot..."
+    sudo systemctl enable sshd && sudo systemctl start sshd
 fi
 
 # nss-mdns is evil
+### don't do this on KDE!!! it removes... KDE :)
 sudo dnf -y remove nss-mdns
 
-# TODO: modify this for Progleasing machines
+# if you decide to run KDE again, you can do this instead:
+#cp /etc/nsswitch.conf ~/nsswitch.conf.bkup
+#sudo sed -i 's/^hosts:.*mdns4_minimal.*/hosts:      files dns myhostname/g' /etc/nsswitch.conf
 
+# keeping this in case I ever need it again...
 # cypher cert from Active Directory
-# wget --quiet --no-check-certificate --http-user=bebrow --ask-password 'https://cypher.pgx.local/certsrv/certnew.cer?ReqID=CACert&Renewal=0&Mode=inst&Enc=b64' -O cypher.crt
-# cat cypher.crt | sudo tee /etc/pki/ca-trust/source/anchors/cypher.crt && sudo update-ca-trust && # rm cypher.crt
-
-# telegram - this will extract to $HOME/Telegram
-# cd $HOME && wget https://telegram.org/dl/desktop/linux -O - | tar Jxv 
+#grep -q stormwind.local /etc/resolv.conf
+#if [ $? -eq 0 ]; then
+#  wget --quiet --no-check-certificate --http-user=benjamin.brown --ask-password 'https://pfprdd1ca01.stormwind.local/certsrv/certnew.cer?ReqID=CACert&Renewal=0&Mode=inst&Enc=b64' -O pfprdd1ca01.crt
+#  cat pfprdd1ca01.crt | sudo tee /etc/pki/ca-trust/source/anchors/pfprdd1ca01.crt && sudo update-ca-trust && rm cypher.crt
+#fi
 
 # if we're running an Intel video chipset, we need to tweak a file so that tearing is reduced drastically
 lspci | grep -qi VGA.*intel
@@ -91,11 +119,14 @@ EndSection
 EOF
 fi
 
-# TODO: add a section to install nvidia drivers if necessary?
-
 # kill firewalld
-sudo systemctl stop firewalld
 sudo systemctl disable firewalld
+sudo systemctl stop firewalld
 
 # virtualbox needs you to be a member of vboxusers if you have a prayer of using USB
 sudo usermod -aG vboxusers $(whoami)
+echo "You should log out now if you want to use USB devices with VirtualBox."
+
+# we're done
+echo "$(basename $0) completed successfully!"
+exit
