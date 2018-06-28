39bfa69e0b2ee53ffc49d49b5639c7bb06c9b60c
Ben Brown <xthor@xthorsworld.com>
2018-06-27 18:37:12 -0600

    needs work: retry loop is broken

--- python/check_rpi_up.py
+++ python/check_rpi_up.py
@@ -8,9 +8,17 @@ import urllib
 from platform import system as system_name  # Returns the system/OS name
 from subprocess import call as system_call  # Execute a shell command
 import subprocess
+import argparse
+
+# we need some arguments provided to us
+parser = argparse.ArgumentParser()
+parser.add_argument('-c', '--pings', help='Number of pings to send', required=True, type=int)
+parser.add_argument('-s', '--sleep', help='How long to sleep between ping sessions', required=True, type=int)
+parser.add_argument('-H', '--host', help='FQDN or IP to ping', required=True)
+args = parser.parse_args()
 
 
-def ping(host):
+def old_ping(host):
     """
     Returns True if host (str) responds to a ping request.
     Remember that a host may not respond to a ping (ICMP) request even if the host name is valid.
@@ -26,24 +34,48 @@ def ping(host):
     return system_call(command) == 0
 
 
+def ping(host):
+    try:
+        subprocess.check_output(
+            ['ping', '-c', str(args.pings), '-w', str(args.pings), '-q', host],
+            stderr=subprocess.STDOUT,  # get all output
+            universal_newlines=True  # return string not bytes
+        )
+        return True
+    except subprocess.CalledProcessError:
+        return False
+
+
+class PingFailed(Exception): pass
+
 # start measuring how long this script has been running!
 start_time = datetime.now()
 
 # loop till status changes
-while True:
-    try:
-        print('{} : Pinging 10.200.105.106/24...'.format(datetime.now()))
-        response = subprocess.check_output(
-            ['ping', '-c', '1', '-w', '1', '10.200.105.106'],
-            stderr=subprocess.STDOUT,  # get all output
-            universal_newlines=True  # return string not bytes
-        )
-        print("Success! Sleeping 5 seconds...")
-        time.sleep(5)
-    except subprocess.CalledProcessError:
-        response = None
-        print('Ping failed!')
-        break
+failcount = 0
+maxfails = 3
+while failcount == 0:
+    print('{} : Pinging {} ({} pings)...'.format(datetime.now(), args.host, args.pings))
+    result = ping(args.host)
+    if result is True:
+        print("Success! Sleeping {} seconds...".format(args.sleep))
+        time.sleep(args.sleep)
+    else:
+        attempt = 1
+        try:
+            print("{} : Pinging host {} failed (attempt {} of {})!".format(datetime.now(), args.host, failcount,
+                                                                           maxfails))
+            print("Sleeping 5 seconds before next attempt...")
+            time.sleep(5)
+            result = ping(args.host)
+            if result is True:
+                print("Success! Resuming normal operations!")
+            else:
+                attempt = attempt + 1
+                if attempt >= maxfails:
+                    raise PingFailed
+        except PingFailed:
+            failcount = 1
 
     # result = ping('10.200.105.106')
     #if result is not True:
@@ -56,9 +88,11 @@ while True:
 time_elapsed = datetime.now() - start_time
 
 # print('Time elapsed (hh:mm:ss.ms) {}'.format(time_elapsed))
-msg_time_elapsed = 'The Raspberry Pi Zero finally died! Time elapsed (hh:mm:ss.ms) {}'.format(time_elapsed)
+msg_time_elapsed = 'Host {} is down! Time elapsed (hh:mm:ss.ms) {}'.format(args.host, time_elapsed)
 
 print(msg_time_elapsed)
+
+print("Sending pushover message...")
 
 # send a pushover message
 conn = httplib.HTTPSConnection("api.pushover.net:443")
