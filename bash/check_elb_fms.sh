b90854e1b13951d6dc3921b2db166c8ebfedcefc
Ben Brown <xthor@xthorsworld.com>
2018-09-20 23:22:40 -0600

    another check script for elb stuff

---
+++ bash/check_elb_fms.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+while true; do 
+  for ip in 10.99.32.238 10.99.0.99; do
+    echo -n "${ip}: "
+    STARTTIME=$(date +%s)
+    curl --write-out %{http_code} --silent --output /dev/null http://${ip}/api/public/v2/leads/ping
+    ENDTIME=$(date +%s)
+    ELAPSED=$(expr ${ENDTIME} - ${STARTTIME})
+    echo -n " elapsed time: ${ELAPSED}"
+    echo
+  done
+  sleep 1
+done
