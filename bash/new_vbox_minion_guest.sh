60d2c84ea868293069162f3d38e6c280ab584eb8
Ben Brown <ben.brown@chghealthcare.com>
2019-02-01 11:59:41 -0700

    made the source template parameterized

--- bash/new_vbox_minion_guest.sh
+++ bash/new_vbox_minion_guest.sh
@@ -5,25 +5,26 @@ function usage() {
 	echo "`basename $0`: Deploy a new VirtualBox VM as a Salt minion."
 	echo "Usage:
 
-`basename $0` -n <name of vm>"
+`basename $0` -n <name of new vm> -s <name of template>"
 	exit 255
 }
 
 # get command-line args
-while getopts "n:" OPTION; do
+while getopts "n:s:" OPTION; do
 	case $OPTION in
-		n) servername="$OPTARG";;
+		n) servername="${OPTARG}";;
+    s) template="${OPTARG}";;
 		*) usage;;
 	esac
 done
 
 # ensure argument was passed
-if [ -z "${servername}" ]; then
+if [ -z "${servername}" -o -z "${template}" ]; then
   usage
 fi
 
 # deploy
-vboxmanage clonevm CentOS\ Template --name ${servername} --register
+vboxmanage clonevm ${template} --name ${servername} --register
 if [ $? -eq 0 ]; then
   vboxmanage guestproperty set ${servername} GuestName ${servername}
   if [ $? -eq 0 ]; then
