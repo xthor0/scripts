c2627007350317256afcf0e4917666f03cc01214
Ben Brown <xthor@xthorsworld.com>
2019-06-02 11:17:26 -0600

    forgot openbox. really??

--- bash/install_openbox_fedora.sh
+++ bash/install_openbox_fedora.sh
@@ -3,6 +3,6 @@
 # this script is bare right now - eventually, I'd like to make it configure everything necessary to run a full-featured Openbox
 # DE on any recent version of Fedora, no matter which platform was installed
 
-sudo dnf install tint2 obconf compton volumeicon nitrogen obmenu xfce4-notifyd conky xscreensaver lxqt-openssh-askpass xfce4-power-manager blueman arandr thunar gmrun leafpad lxappearance nm-tray lxappearance network-manager-applet xbacklight
+sudo dnf install openbox tint2 obconf compton volumeicon nitrogen obmenu xfce4-notifyd conky xscreensaver lxqt-openssh-askpass xfce4-power-manager blueman arandr thunar gmrun leafpad lxappearance nm-tray lxappearance network-manager-applet xbacklight
 
 # find the compton.conf and openbox autostart in the dotfiles directory - should be all you need
