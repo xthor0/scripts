2a9b97cede71cf51a763f783657ffa7d33f476b1
Ben Brown <xthor@xthorsworld.com>
2019-11-14 20:05:14 -0700

    wip

--- bash/fedora_openbox_setup.sh
+++ bash/fedora_openbox_setup.sh
@@ -205,6 +205,9 @@ fi
 sudo systemctl disable firewalld
 sudo systemctl stop firewalld
 
+# make openbox default for LXDM
+sudo sed -i 's/^# session=/usr/bin/startlxde/session=/bin/openbox/g' /etc/lxdm/lxdm.conf
+
 ############
 # END
 ############
