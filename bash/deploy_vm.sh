7475e1bbe0671568365ae47ebd86cce5c694b826
Ben Brown <xthor@xthorsworld.com>
2019-11-21 22:01:02 -0700

    wip

--- bash/deploy_vm.sh
+++ bash/deploy_vm.sh
@@ -192,6 +192,7 @@ instance-id: 1
 local-hostname: ${vmname}
 EOF
   # generate the password with this command: mkpasswd --method=SHA-512 --stdin
+  # ESCAPE YOUR DOLLAR SIGNS or stuff won't work!
   cat << EOF > ${TEMP_D}/user-data
 #cloud-config
 users:
@@ -199,12 +200,7 @@ users:
     passwd: \$6\$w6ZFMnTUXqAniT9h\$0qwEbOsSRmI4alw6CZTB/.6i89GObwMk/yit7SaNSxvM10ENTIEjK0Pvl.4eC3tzGq1Dd81SKoyxdPSpiLM100
     lock_passwd: false
     primary_group: xthor
-    groups: wheel
-    ssh_authorized_keys:
-      - ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDSUppn5b2njEQSw8FHqyZ0OZiPD14wEejulwnQ7gxLdQYJEqXMleHx4u/9ff3/jDXoGaBFiT2LmUTnpMV8HSj4jsB4PCoFAbq4XnlnwyBx7va/8LQOMdKsjF5W6peO+DYKh+ow9YaJvctzGPebkkNvhI0YFhZod58uoO7lyTnQXkMm8DXl6q7WhNfsZZiwr7tXicUZojU0msMiDpX1JvhGow+mKym0U/6cMgozypYfNbQ2PVkfNnadslp29O5Mfd5X4U+cbACa1sUYYqOT2Zz8C4t5QFXRY1LNokmRbcqbO01bygbE4S2TDnvRz+XZmfZTuw9MMgp7JPfo6cOfDYKf xthor
-  - name: root
-    passwd: \$6\$w6ZFMnTUXqAniT9h\$0qwEbOsSRmI4alw6CZTB/.6i89GObwMk/yit7SaNSxvM10ENTIEjK0Pvl.4eC3tzGq1Dd81SKoyxdPSpiLM100
-    lock_passwd: false
+    sudo: ALL=(ALL) NOPASSWD:ALL
     ssh_authorized_keys:
       - ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDSUppn5b2njEQSw8FHqyZ0OZiPD14wEejulwnQ7gxLdQYJEqXMleHx4u/9ff3/jDXoGaBFiT2LmUTnpMV8HSj4jsB4PCoFAbq4XnlnwyBx7va/8LQOMdKsjF5W6peO+DYKh+ow9YaJvctzGPebkkNvhI0YFhZod58uoO7lyTnQXkMm8DXl6q7WhNfsZZiwr7tXicUZojU0msMiDpX1JvhGow+mKym0U/6cMgozypYfNbQ2PVkfNnadslp29O5Mfd5X4U+cbACa1sUYYqOT2Zz8C4t5QFXRY1LNokmRbcqbO01bygbE4S2TDnvRz+XZmfZTuw9MMgp7JPfo6cOfDYKf xthor
 timezone: America/Denver
