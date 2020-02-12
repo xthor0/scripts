4bf86f204787cb66b0e2ef7fd3e102c2cf041f86
Ben Brown <bbrown@nav.com>
2020-02-12 13:23:45 -0700

    wip

--- bash/cent7_atomic_vm.sh
+++ bash/cent7_atomic_vm.sh
@@ -25,13 +25,14 @@ fi
 
 gateway=10.200.106.1
 
-# image source
+# image sources
 srcimg=/home/xthor/cloudimage/CentOS-Atomic-Host-7.1910-GenericCloud.qcow2
 dstimg=/var/lib/libvirt/images/${vmname}.qcow2
+ciimg=/var/lib/libvirt/images/${vmname}-ci.qcow2
 
 # http root
-docroot=/var/www/lighttpd/ci
-cidata="${docroot}/${vmname}"
+#docroot=/var/www/lighttpd/ci
+#cidata="${docroot}/${vmname}"
 
 # clone the image
 if [ -f ${dstimg} ]; then
@@ -54,7 +55,8 @@ else
 fi
 
 # create dir for ci data
-mkdir ${cidata}
+#mkdir ${cidata}
+cidata=$(mktemp -d)
 if [ $? -ne 0 ]; then
   echo "error creating ${vmname} dir in ${docroot} -- exiting."
   exit 255
@@ -107,6 +109,11 @@ if [ $? -ne 0 ]; then
     exit 255
 fi
 
+# make the disk for ci data
+truncate --size 2M ${ciimg}
+mkfs.vfat -n cidata ${ciimg}
+mcopy -oi ${ciimg} ${cidata}/user-data ${cidata}/meta-data ::
+
 # make sure the VM doesn't already exist (though, it should have died earlier when creating the cidata dir)
 virsh list --all --name | grep -qw ${vmname}
 if [ $? -eq 0 ]; then
@@ -115,4 +122,4 @@ if [ $? -eq 0 ]; then
 fi
 
 # build the vm
-virt-install --virt-type=kvm --name ${vmname} --ram 2048 --vcpus 1 --os-variant=rhel-atomic-7.4 --network=bridge=br-vlan06,model=virtio --graphics vnc --disk ${dstimg},cache=writeback --import --noautoconsole --sysinfo type=smbios,serial=ds=nocloud-net;s=http://10.200.106.12/${vmname}/
+virt-install --virt-type=kvm --name ${vmname} --ram 2048 --vcpus 1 --os-variant=rhel-atomic-7.4 --network=bridge=br-vlan06,model=virtio --graphics vnc --disk ${dstimg},cache=writeback --disk=${ciimg} --import --noautoconsole
