295f0d39de18d4db342d30a7b131e89db7476bdf
Ben Brown <bbrown@nav.com>
2020-02-14 11:54:12 -0700

    wip

--- python/ping_check.py
+++ python/ping_check.py
@@ -5,6 +5,7 @@ import subprocess
 import argparse
 import logging
 from os.path import expanduser
+import signal
 
 # we need some arguments provided to us
 parser = argparse.ArgumentParser()
@@ -29,9 +30,19 @@ def ping(host):
 
 class PingFailed(Exception): pass
 
+class GracefulKiller:
+  kill_now = False
+  def __init__(self):
+    signal.signal(signal.SIGINT, self.exit_gracefully)
+    signal.signal(signal.SIGTERM, self.exit_gracefully)
+
+  def exit_gracefully(self,signum, frame):
+    self.kill_now = True
+
+
 # we log to $HOME
 home = expanduser("~")
-logfile = "{}/pingcheck.log".format(home)
+logfile = "{}/{}.log".format(home,args.host)
 
 if args.verbose is False:
   print('Ping check of host {} started. Logging to file {}.'.format(args.host, logfile))
@@ -44,52 +55,53 @@ hdlr.setFormatter(formatter)
 logger.addHandler(hdlr) 
 logger.setLevel(logging.INFO)
 
-# write a log message
+# log app starting
 logger.info('App started')
 
-# loop forever
-status = None
-lastStatus = None
-while True:
-  # print out a message if we're running for the first time
-  if lastStatus is None:
-    message = 'Pinging {} ({} pings)...'.format(args.host, args.pings)
-    if args.verbose is True:
-      print(message)
-    logger.info(message)
-  
-  # ping the host and record result
-  result = ping(args.host)
-
-  if result is True:
-    status = "UP"
-  else:
-    status = "DOWN"
-
-  # now check if the status has changed
-  if lastStatus is None:
-    message = "Host {} is {}!".format(args.host, status)
-    if args.verbose is True:
-      print(message)
-    logger.info(message)
-    lastStatus = status
-  else:
-    if status == lastStatus:
-      # there has been no change, so all we do is log it
-      message = "Host {} : No change (still {})".format(args.host, status)
-      logger.info(message)
-    else:
-      message = "Host {} status has changed from {} to {}!".format(args.host, lastStatus, status)
+# ping away
+if __name__ == '__main__':
+  status = None
+  lastStatus = None
+  killer = GracefulKiller()
+  while not killer.kill_now:
+    # print out a message if we're running for the first time
+    if lastStatus is None:
+      message = 'Pinging {} ({} pings)...'.format(args.host, args.pings)
       if args.verbose is True:
         print(message)
-      logger.warning(message)
+      logger.info(message)
+    
+    # ping the host and record result
+    result = ping(args.host)
+  
+    if result is True:
+      status = "UP"
+    else:
+      status = "DOWN"
+  
+    # now check if the status has changed
+    if lastStatus is None:
+      message = "Host {} is {}!".format(args.host, status)
+      if args.verbose is True:
+        print(message)
+      logger.info(message)
+      lastStatus = status
+    else:
+      if status == lastStatus:
+        # there has been no change, so all we do is log it
+        message = "Host {} : No change (still {})".format(args.host, status)
+        logger.info(message)
+      else:
+        message = "Host {} status has changed from {} to {}!".format(args.host, lastStatus, status)
+        if args.verbose is True:
+          print(message)
+        logger.warning(message)
+  
+    # set lastStatus to status
+    lastStatus = status
+  
+    # sleep
+    time.sleep(args.sleep)
 
-  # set lastStatus to status
-  lastStatus = status
-
-  # sleep
-  time.sleep(args.sleep)
-
-
-# die
-exit(0)
+  # log a message and exit
+  logger.info('App stopped')
