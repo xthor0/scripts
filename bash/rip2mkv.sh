d9f1ef5b7b88154d9a0c7f8ec21582c8691edffe
Ben Brown <xthor@xthorsworld.com>
2021-01-02 15:11:35 -0700

    updates

--- bash/rip2mkv.sh
+++ bash/rip2mkv.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+function cleanup_exit() {
+	rm -f ${discinfo}
+	exit 0
+}
+
 # create temp file
 echo "Scanning disc for title, please wait..."
 discinfo=$(mktemp)
@@ -9,13 +14,10 @@ makemkvcon --progress=-stdout -r info dev:/dev/sr0 > $discinfo
 title=$(cat $discinfo | grep '^DRV:0' | cut -d \, -f 6 | tr -d \")
 directory="${HOME}/videos/rips/${title}"
 
-# delete temp file
-rm -f $discinfo
-
 # if title is not set, exit
 if [ -z "$title" ]; then
 	echo "Title could not be determined from disc - exiting."
-	exit 255
+	cleanup_exit
 fi
 
 startDate=$(date)
@@ -24,24 +26,22 @@ if [ ! -d "${directory}" ]; then
 	mkdir "${directory}"
 	if [ $? -ne 0 ]; then
 		echo "Error: Could not create directory ${directory}"
-		exit 255
+		cleanup_exit
 	fi
 else
 	echo "Directory already exists: ${directory} -- exiting!"
-	exit 255
+	cleanup_exit
 fi
 
 pushd "${directory}"
 
+# save the discinfo file and then delete it (for debugging purposes)
+cp ${discinfo} discinfo.txt
+
 # begin ripping disk
 # cribbed this from here: https://gist.github.com/tacofumi/3041eac2f59da7a775c6
-# 4800s is too long for TV shows, found out the hard way...
-#makemkvcon --progress=-stdout --minlength=4800 -r --decrypt --directio=true mkv dev:/dev/sr0 all .
 makemkvcon --progress=-stdout --minlength=600 -r --decrypt --directio=true mkv dev:/dev/sr0 all .
 endDate=$(date)
-
-#$HOME/Dropbox/projects/scripts/prowl.pl -apikey=fb18cb558102482e883ac76ba05a3c1b00212e96 -application=MakeMKV -event="MakeMKV rip completed" -notification="Started: $startDate :: Ended: $endDate" -priority=-2
-#$HOME/Dropbox/projects/scripts/nma.sh MakeMKV "MakeMKV rip completed" "Started: $startDate :: Ended: $endDate" -2
 
 # send message to Pushover indicating that this rip has completed
 curl -s \
@@ -52,4 +52,4 @@ curl -s \
 
 popd
 
-exit 0
+cleanup_exit
