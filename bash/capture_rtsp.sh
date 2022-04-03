132536cf59735dbb7026d442a0b956de01ea606d
Ben Brown <xthor@xthorsworld.com>
2022-04-02 21:48:09 -0600

    updates

--- bash/capture_rtsp.sh
+++ bash/capture_rtsp.sh
@@ -20,6 +20,7 @@ while true; do
 # I think I got all the cases?
 minute=$(date +%M)
 next_hour_mark=$(date +%H)
+day="today"
 if [ ${minute} -lt 15 ]; then
     next_minute_mark=15
 elif [ ${minute} -lt 30 -a ${minute} -ge 15 ]; then
@@ -28,9 +29,10 @@ elif [ ${minute} -lt 45 -a ${minute} -ge 30 ]; then
     next_minute_mark=45
 else
     if [ ${next_hour_mark} -eq 23 ]; then
-        let next_hour_mark=00
+        next_hour_mark=0
+        day="tomorrow"
     else
-        let next_hour_mark+=1
+        ((next_hour_mark=next_hour_mark + 1))
     fi
     next_minute_mark=0
 fi
@@ -38,7 +40,7 @@ fi
 #echo "DEBUGGING: next minute mark is ${next_minute_mark}, next hour mark is ${next_hour_mark}"
 
 seconds_now=$(date +%s)
-seconds_at_next_mark=$(date +%s -d "today ${next_hour_mark}:${next_minute_mark}")
+seconds_at_next_mark=$(date +%s -d "${day} ${next_hour_mark}:${next_minute_mark}")
 ((seconds_left = seconds_at_next_mark - seconds_now))
 
 #echo "DEBUGGING: next minute mark is ${next_minute_mark}, next hour mark is ${next_hour_mark}"
@@ -53,7 +55,24 @@ echo "Running OpenRTSP for ${seconds_left} seconds... and naming the file ${file
 #echo "DEBUGGING: sleeping instead"
 #sleep ${seconds_left}
 
-openRTSP -B 10000000 -b 10000000 -4 -d ${seconds_left} -w 1920 -h 1080 -t rtsp://${cam_ip}/live0 2>> openRTSP.log > ${filename}
+# we know how long we told this to run - so let's push it to the BG and monitor
+openRTSP -B 10000000 -b 10000000 -4 -d ${seconds_left} -w 1920 -h 1080 -t rtsp://${cam_ip}/live0 2>> openRTSP_${cam_name}.log > ${filename} &
+pid=$!
+
+seconds_at_bg=$(date +%s)
+while [ -d /proc/${pid} ]; do
+    # things I might want to check in here...
+    # is the size of the file increasing? If not, either OpenRTSP is stuck, or RTSP on the camera has stopped working
+    # are we past the ${seconds_left} in elapsed time? if so, OpenRTSP might just need to be kicked and restarted
+    seconds_now=$(date +%s)
+    ((elapsed=seconds_now-seconds_at_bg))
+    if [ ${elapsed} -gt ${seconds_left} ]; then
+        echo "OpenRTSP has been running longer than it should have been..."
+        sleep 5
+        kill -HUP1 ${pid}
+    fi
+    sleep 1
+done
 
 # we're going to have to do SOMETHING in the background, to see if the RTSP stream has stopped - I've had this problem a couple of times where RTSP has to be reset on the camera itself...
 
