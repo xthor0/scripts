8677f7a1611b212f6568b4060fd573d4290bd9bb
Ben Brown <xthor@xthorsworld.com>
2022-10-13 23:03:24 -0600

    benchmark

--- bash/sysbench.sh
+++ bash/sysbench.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+for util in fio sysbench; do
+	type ${util} >& /dev/null
+	if [ $? -ne 0 ]; then
+		echo "Missing ${util} :: exiting."
+		exit 255
+	fi
+done
+
+
 if [ "$(uname -s)" == "Darwin" ]; then
 	cpu_model="$(sysctl -n machdep.cpu.brand_string)"
 else
@@ -18,3 +28,46 @@ sysbench memory run | egrep -i '(total operations|transferred)'
 echo
 echo "Memory Test (Multi-thread)"
 sysbench --threads=$(nproc) memory run | egrep -i '(total operations|transferred)'
+
+echo
+echo 
+
+echo "Creating fio test"
+if test -d ${HOME}/fio; then
+	echo "dir ${HOME}/fio already exists"
+else
+	mkdir ${HOME}/fio
+	echo "created ${HOME}/fio"
+fi
+
+cat << EOF > ${HOME}/fio/ssd-test.fio
+[global]
+bs=4k
+ioengine=libaio
+iodepth=4
+size=10g
+direct=1
+runtime=60
+directory=${HOME}/fio
+filename=ssd.test.file
+
+[seq-read]
+rw=read
+stonewall
+
+[rand-read]
+rw=randread
+stonewall
+
+[seq-write]
+rw=write
+stonewall
+
+[rand-write]
+rw=randwrite
+stonewall
+EOF
+
+fio ${HOME}/fio/ssd-test.fio
+
+rm -rf ${HOME}/fio/
