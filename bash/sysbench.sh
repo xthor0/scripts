76de075f8d4890d713bd1294e9cc0a2d1d422ff7
Ben Brown <xthor@xthorsworld.com>
2025-02-23 16:50:57 -0700

    now works with raspberry pi

--- bash/sysbench.sh
+++ bash/sysbench.sh
@@ -3,8 +3,16 @@ if [ "$(uname -s)" == "Darwin" ]; then
 	cpu_model="$(sysctl -n machdep.cpu.brand_string)"
 	nproc=$(sysctl -n hw.physicalcpu)
 else
-	cpu_model="$(grep ^model\ name /proc/cpuinfo  | head -n1 | cut -d \: -f 2 | cut -b 2-)"
-	nproc=$(nproc)
+  # intel models can get the CPU this way
+  nproc=$(nproc)
+  case "$(uname -p)" in
+    "x86_64")
+      cpu_model="$(grep ^model\ name /proc/cpuinfo  | head -n1 | cut -d \: -f 2 | cut -b 2-)"
+      ;;
+    "aarch64")
+      cpu_model="$(grep ^Model /proc/cpuinfo | cut -b 10-)"
+      ;;
+  esac
 fi
 
 echo "CPU Model: ${cpu_model} :: ${nproc} CPU Cores"
